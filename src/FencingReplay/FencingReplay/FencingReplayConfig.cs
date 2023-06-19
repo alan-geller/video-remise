dae9ec6798b73197d0338bc3593477416b4ee0c7
Alan Geller <ageller@microsoft.com>
2023-06-19 15:35:38 -0700

    WIP adding columns to the GIU

---
+++ src/FencingReplay/FencingReplay/FencingReplayConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace FencingReplay
+{
+    internal class MediaSourceInfo
+    {
+        string groupId;
+        string groupDisplayName;
+        string sourceId;
+
+    }
+
+    internal class FencingReplayConfig
+    {
+        List<MediaSourceInfo> videoSources;
+        List<MediaSourceInfo> audioSources;
+
+        void ToFile(string filePath)
+        {
+            var serializer = new YamlDotNet.Serialization.SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+            File.WriteAllText(filePath, serializer.Serialize(this));
+        }
+
+        static FencingReplayConfig FromFile(string filePath)
+        {
+            var deserializer = new YamlDotNet.Serialization.DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+            var myConfig = deserializer.Deserialize<FencingReplayConfig>(File.ReadAllText(filePath));
+
+            return myConfig;
+        }
+    }
+}
--- src/FencingReplay/FencingReplay/MainPage.xaml.cs
+++ src/FencingReplay/FencingReplay/MainPage.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
 using Windows.Media.Capture.Frames;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,25 +28,136 @@ namespace FencingReplay
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        class VideoChannel
+        {
+            MainPage mainPage;
+            int gridColumn;
+            ListBox sourceSelector;
+            CaptureElement captureElement;
+            MediaFrameSourceGroup currentSource;
+            MediaCapture currentCapture;
+            DisplayRequest currentRequest;
+
+            static IReadOnlyList<MediaFrameSourceGroup> CurrentSources;
+
+            internal VideoChannel(MainPage page, int column)
+            {
+                mainPage = page;
+                gridColumn = column;
+                sourceSelector = new ListBox();
+                PopulateSourceList(sourceSelector);
+                mainPage.LayoutGrid.Children.Add(sourceSelector);
+                Grid.SetColumn(sourceSelector, gridColumn);
+                Grid.SetRow(sourceSelector, 0);
+                captureElement = new CaptureElement();
+                mainPage.LayoutGrid.Children.Add(captureElement);
+                Grid.SetColumn(captureElement, gridColumn);
+                Grid.SetRow(captureElement, 1);
+            }
+
+            async void SetSource(string displayName)
+            {
+                var sourceGroup = FindMediaSource(displayName);
+            }
+
+            static MediaFrameSourceGroup FindMediaSource(string displayName)
+            {
+                // This method will never get called when CurrentSources is uninitialized
+                foreach (var frameSource in CurrentSources)
+                {
+                    if (displayName == frameSource.DisplayName)
+                    {
+                        return frameSource;
+                    }
+                }
+                return null;
+            }
+
+            static async void PopulateSourceList(ListBox listBox)
+            {
+                if (CurrentSources == null)
+                {
+                    CurrentSources = await MediaFrameSourceGroup.FindAllAsync();
+
+                    //FileSavePicker savePicker = new FileSavePicker();
+                    //savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+                    //savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+                    //savePicker.SuggestedFileName = "sources";
+
+                    //StorageFile file = await savePicker.PickSaveFileAsync();
+                    //if (file != null)
+                    //{
+                    //    using (var buffer = new StringWriter())
+                    //    {
+                    //        foreach (var frameSource in frameSources)
+                    //        {
+                    //            buffer.WriteLine($"Source group {frameSource.DisplayName}:");
+                    //            buffer.WriteLine($"  ID: {frameSource.Id}");
+                    //            foreach (var src in frameSource.SourceInfos)
+                    //            {
+                    //                buffer.WriteLine($"    Source ID: {src.Id}");
+                    //                buffer.WriteLine($"      Source Kind: {src.SourceKind}");
+                    //                buffer.WriteLine($"      Stream type: {src.MediaStreamType}");
+                    //            }
+                    //        }
+                    //        await FileIO.WriteTextAsync(file, buffer.ToString());
+                    //    }
+                    //}    
+                }
+
+                listBox.Items.Clear();
+                foreach (var frameSource in CurrentSources)
+                {
+                    listBox.Items.Add(frameSource.DisplayName);
+                } 
+            }
+        }
+
+        bool playing = false;
+        List<VideoChannel> channels;
+        
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            channels = new List<VideoChannel>();
+            channels.Add(new VideoChannel(this, 0));
+            channels.Add(new VideoChannel(this, 1));
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            PopulateSourceList();
         }
 
-        private async void PopulateSourceList()
+        private void ListBox1_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var frameSources = await MediaFrameSourceGroup.FindAllAsync();
-            listBox.Items.Clear();
+            var selection = ListBox1.SelectedItem?.ToString();
+            SetVideoSource(Capture1, selection);
+        }
+
+        private void ListBox2_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private MediaFrameSourceGroup FindMediaSource(string displayName)
+        {
+            // We have to use the blocking GetResults to transition from asynch to synch
+            var frameSources = MediaFrameSourceGroup.FindAllAsync().GetResults();
             foreach (var frameSource in frameSources)
             {
-                listBox.Items.Add(frameSource.DisplayName);
+                if (displayName == frameSource.DisplayName)
+                {
+                    return frameSource;
+                }
             }
+            return null;
+        }
+
+        private void SetVideoSource(CaptureElement captureElement, string displayName)
+        {
+
         }
     }
 }
