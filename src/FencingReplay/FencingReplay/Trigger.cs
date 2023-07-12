00de6c7878f08718a9a4910d865ad46fbd9962b9
Alan Geller <ageller@microsoft.com>
2023-07-12 14:18:42 -0700

    WIP Starting on device config and scoring machine interface

---
+++ src/FencingReplay/FencingReplay/Trigger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FencingReplay
+{
+    internal abstract class Trigger
+    {
+        public struct LightEventArgs
+        {
+            bool Red;
+            bool LeftWhite;
+            bool Green;
+            bool RightWhite;
+        }
+
+        public bool FiresClockEvents { get; set; }
+
+        public event EventHandler OnClockStart;
+        public event EventHandler OnClockStop;
+        public event EventHandler<LightEventArgs> OnLight;
+
+        protected void FireClockStartEvent()
+        {
+            var temp = OnClockStart;
+            if (temp != null)
+            {
+                temp(this, new EventArgs());
+            }
+        }
+        protected void FireClockStopEvent()
+        {
+            var temp = OnClockStop;
+            if (temp != null)
+            {
+                temp(this, new EventArgs());
+            }
+        }
+        protected void FireLightEvent(LightEventArgs args)
+        {
+            var temp = OnLight;
+            if (temp != null)
+            {
+                temp(this, args);
+            }
+        }
+    }
+}
