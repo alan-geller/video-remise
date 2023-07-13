0bb336c5532eb018c094fbe6886ad7a427aeba88
Alan Geller <ageller@microsoft.com>
2023-07-13 15:38:41 -0700

    Started on the device config page

--- src/FencingReplay/FencingReplay/ConfigPage.xaml.cs
+++ src/FencingReplay/FencingReplay/ConfigPage.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace FencingReplay
 {
-    public sealed partial class ConfigPage : ContentDialog
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ConfigPage : Page
     {
         public ConfigPage()
         {
             this.InitializeComponent();
-        }
-
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-        }
-
-        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
         }
     }
 }
