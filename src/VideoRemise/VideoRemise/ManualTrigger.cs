30f74fdd3c677e3f3785e0d8d92a2aa95376c27b
Alan Geller <ageller@microsoft.com>
2023-08-08 17:44:00 -0700

    WIP Manual trigger

---
+++ src/VideoRemise/VideoRemise/ManualTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Input;
+using Windows.System;
+using LightDisplayVisualEffect;
+
+namespace VideoRemise
+{
+    internal class ManualTrigger : Trigger
+    {
+        private MainPage mainPage;
+        private bool clockRunning = false;
+
+        ManualTrigger(MainPage main)
+        {
+            mainPage = main;
+            mainPage.KeyDown += OnKeyDown;
+        }
+
+        private void OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case VirtualKey.Space:
+                    if (clockRunning)
+                    {
+                        FireClockStopEvent();
+                    }
+                    else
+                    {
+                        FireClockStartEvent();
+                    }
+                    break;
+                case VirtualKey.LeftShift:
+                    FireLightEvent(Lights.Red);
+                    break;
+                case VirtualKey.LeftControl:
+                    FireLightEvent(Lights.LeftWhite);
+                    break;
+                case VirtualKey.RightShift:
+                    FireLightEvent(Lights.Green);
+                    break;
+                case VirtualKey.RightControl:
+                    FireLightEvent(Lights.RightWhite);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override Task Initialize(VideoRemiseConfig config)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
